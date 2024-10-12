5e4bfb348f9150f76347c746ab1c77d759fa4983
Mikhail <mtarantin534@gmail.com>
2024-10-12 17:45:11 +0400

    pagination

--- movie/src/pages/Main/Main.tsx
+++ movie/src/pages/Main/Main.tsx
@@ -3,11 +3,18 @@ import MovieList from '../../components/MovieList/MovieList'
 import Movie from './../../type/type'
 import { Container } from '@mui/material'
 import Search from '../../components/Search/Search'
+import axios from 'axios'
+import { Pagination } from '@mui/material'
 
 const Main = () => {
 	const [movieList, setMovieList] = useState<Movie[]>([])
 	const [fullMovieList, setFullMovieList] = useState<Movie[]>([])
 	const [search, setSearch] = useState('')
+	const [loading, setLoading] = useState(true)
+	const [error, setError] = useState(null)
+	const [currentPage, setCurrentPage] = useState(1)
+	const itemsPerPage = 12
+	const [totalPages, setTotalPages] = useState(0)
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const value = e.target.value.toLowerCase()
@@ -24,27 +31,52 @@ const Main = () => {
 		}
 	}
 
-	const getMovie = () => {
-		fetch(
-			'https://api.themoviedb.org/3/discover/movie?api_key=18876dba15b1b878cd5231e198aa91c0'
-		)
-			.then(res => res.json())
-			.then(json => {
-				setMovieList(json.results)
-				setFullMovieList(json.results)
-			})
+	const getMovie = async (page: number) => {
+		try {
+			const response = await axios.get(
+				`https://api.themoviedb.org/3/movie/popular`,
+				{
+					params: {
+						api_key: '18876dba15b1b878cd5231e198aa91c0',
+						page: page,
+					},
+				}
+			)
+			setMovieList(response.data.results)
+			setFullMovieList(response.data.results)
+			setTotalPages(response.data.total_pages)
+			setLoading(false)
+		} catch (err: any) {
+			setError(err)
+			setLoading(false)
+		}
 	}
 
 	useEffect(() => {
-		getMovie()
-	}, [])
+		getMovie(currentPage)
+	}, [currentPage])
+
+	const handlePageChange = (
+		event: React.ChangeEvent<unknown>,
+		value: number
+	) => {
+		setCurrentPage(value)
+	}
 
 	return (
 		<Container
-			sx={{ display: 'flex', flexWrap: 'Wrap', gap: '34px', marginTop: '20px' }}
+			sx={{ display: 'flex', flexWrap: 'wrap', gap: '34px', marginTop: '20px' }}
 		>
 			<Search value={search} onChange={handleChange} />
-			<MovieList movieList={movieList} />
+			<MovieList movieList={movieList.slice(0, itemsPerPage)} />
+			<Pagination
+				count={totalPages}
+				variant='outlined'
+				shape='rounded'
+				page={currentPage}
+				onChange={handlePageChange}
+				color='primary'
+			/>
 		</Container>
 	)
 }
