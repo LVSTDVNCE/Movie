98e82cfbe2b78f5ae80fa41733ee58ff20ee6599
Mikhail <mtarantin534@gmail.com>
2024-10-13 23:31:32 +0400

    add new page MovieAbout

--- movie/src/App.tsx
+++ movie/src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Layout from './components/Layout/Layout'
 import Main from './pages/Main/Main'
+import MovieAbout from './pages/MovieAbout/MovieAbout'
 
 function App() {
 	return (
 		<Routes>
 			<Route path='/' element={<Layout />}>
 				<Route index element={<Main />} />
+				<Route path='MovieAbout' element={<MovieAbout />} />
 			</Route>
 		</Routes>
 	)
--- movie/src/components/MovieListCard/MovieListCard.tsx
+++ movie/src/components/MovieListCard/MovieListCard.tsx
@@ -8,6 +8,7 @@ import {
 	Button,
 } from '@mui/material'
 import MovieCard from './../../type/type'
+import { Link } from 'react-router-dom'
 
 interface MovieListCard {
 	movie: MovieCard
@@ -16,13 +17,16 @@ interface MovieListCard {
 const MovieListCard: React.FC<MovieListCard> = ({ movie }) => {
 	const imageLink = 'https://image.tmdb.org/t/p/w500'
 	return (
-		<Card sx={{ maxWidth: 345 }} key={movie.id}>
+		<Card
+			key={movie.id}
+			sx={{ display: 'flex', position: 'relative', maxWidth: '100%' }}
+		>
 			<CardMedia
 				component='img'
 				alt={movie.title}
 				height='140'
 				image={`${imageLink}${movie.poster_path}`}
-				sx={{ objectFit: 'cover' }}
+				sx={{ objectFit: 'contain', maxWidth: 300, height: 300 }}
 			/>
 			<CardContent>
 				<Typography gutterBottom variant='h5' component='div'>
@@ -33,8 +37,12 @@ const MovieListCard: React.FC<MovieListCard> = ({ movie }) => {
 				</Typography>
 			</CardContent>
 			<CardActions>
-				<Button size='small'>Share</Button>
-				<Button size='small'>Learn More</Button>
+				<Button
+					size='small'
+					sx={{ position: 'absolute', bottom: 20, right: 20 }}
+				>
+					<Link to='/MovieAbout'>Watch</Link>
+				</Button>
 			</CardActions>
 		</Card>
 	)
